3970d573fde7eda55162548a1befa2391451286e
Hassan Khokhar <tauqeerhassan66@gmail.com>
2018-06-10 00:31:03 +0500

    added build

--- Solidity_Contract/VotingInstance.js
+++ Solidity_Contract/VotingInstance.js
@@ -1,4 +1,4 @@
 import web3 from './web3';
 import * as VotingSystem from './build/VotingSystem.json';
 
-export default new web3.eth.Contract(JSON.parse(VotingSystem.interface), "0xf11bD3ffC6b8c3B9425047b52CF450F90bc5F5dc");
+export default new web3.eth.Contract(JSON.parse(VotingSystem.interface), "0xAF5FB00558115E004c45C218667213aC54bb46b7");
